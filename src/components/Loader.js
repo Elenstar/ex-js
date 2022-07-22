f52ce037b83eb634a26320f0ef864a904fc93311
Elenstar <es89055330598@yandex.ru>
2022-07-22 18:15:22 +0300

    DIP

---
+++ src/components/Loader.js
@@ -0,0 +1,20 @@
+import {$} from '../core/dom'
+
+export function Loader() {
+    return $.create('dev', 'loader').html(`
+        <div class="lds-spinner">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    `)
+}
--- src/core/routes/Router.js
+++ src/core/routes/Router.js
@@ -1,5 +1,6 @@
 import {$} from '../dom'
 import {ActiveRoute} from './ActiveRoute'
+import {Loader} from '../../components/Loader'
 
 export class Router {
     constructor(selector, routes) {
@@ -8,6 +9,8 @@ export class Router {
         }
         this.$placeholder = $(selector)
         this.routes = routes
+
+        this.loader = new Loader()
 
         this.page = null
 
@@ -21,12 +24,12 @@ export class Router {
         this.changePageHandler()
     }
 
-    changePageHandler() {
+    async changePageHandler() {
         if (this.page) {
             this.page.destroy()
         }
 
-        this.$placeholder.clear()
+        this.$placeholder.clear().append(this.loader)
 
         const Page = ActiveRoute.path.includes('ex')
             ? this.routes.ex
@@ -34,7 +37,9 @@ export class Router {
 
         this.page = new Page(ActiveRoute.param)
 
-        this.$placeholder.append(this.page.getRoot())
+        const root = await this.page.getRoot()
+
+        this.$placeholder.clear().append(root)
 
         this.page.afterRender()
     }
